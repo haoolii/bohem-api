b6a481a9004505d1ee90f0d1281e2721dd12f51e
unnhao <unnhao@gmail.com>
2025-01-24 17:51:58 +0800

    feat: update schema

--- src/schema/shorten.schema.ts
+++ src/schema/shorten.schema.ts
@@ -2,8 +2,8 @@ import { z } from "zod";
 
 export const PostShortenBodySchema = z.object({
     original: z.string(),
-    prompt: z.string().optional(),
-    password: z.string().optional(),
+    prompt: z.string().optional().nullable(),
+    password: z.string().nullable(),
     passwordRequired: z.boolean(),
     type: z.enum(["image", "video", "url"]),
     expireIn: z.number().int().positive(),
